a7b21f524a448a9681903cfc28671965debb9cb8
Andrew <andrew@lobos.me>
2016-11-01 14:41:56 -0400

    Forgot to update these includes

--- include/motion_control/motion_control.h
+++ include/motion_control/motion_control.h
@@ -1,17 +1,17 @@
 #ifndef MOTION_CONTROL
 #define MOTION_CONTROL
 
-#include "encoder.h"
-#include "motor.h"
+#include <motion_control/encoder.h>
+#include <motion_control/motor.h>
 
 #include <motion_control/fixed_size_stack.h>
 #include <motion_control/time.h>
 
 
-#include "profile_point.h"
-#include "motion_profile_1d.h"
-#include "pid_controller.h"
+#include <motion_control/profile_point.h>
+#include <motion_control/motion_profile_1d.h>
+#include <motion_control/pid_controller.h>
 
-#include "trapezoidal_motion_profile.h"
+#include <motion_control/trapezoidal_motion_profile.h>
 
 #endif
