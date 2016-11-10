1393882e7e74198fcfa57ca7e7c832753190a621
Andrew <andrew@lobos.me>
2016-11-10 13:33:29 -0500

    Provide mircosecond resolution on Windows

--- src/platforms/Windows/time.cpp
+++ src/platforms/Windows/time.cpp
@@ -5,7 +5,26 @@ namespace MotionControl
 {
 double Timer::now()
 {
-    return (double)GetTickCount() / 1000.0;
+    static LARGE_INTEGER start;
+    static LARGE_INTEGER freq;
+    static bool init = false;
+    if ( !init )
+    {
+        init = true;
+        QueryPerformanceFrequency(&freq);
+        QueryPerformanceCounter(&start);
+    }
+
+    LARGE_INTEGER now;
+    QueryPerformanceCounter(&now);
+
+    LARGE_INTEGER elapsed;
+    elapsed.QuadPart = now.QuadPart - start.QuadPart;
+
+    elapsed.QuadPart *= 1000000;
+    elapsed.QuadPart /= freq.QuadPart;
+
+    return elapsed.QuadPart / 1e6;
 }
 
 void Timer::delay(double t)
